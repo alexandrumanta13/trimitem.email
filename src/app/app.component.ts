0559e592192b2a4ba630b764526bde9a39d8bbc6
Alexandru Manta <alexandru.manta@ing.com>
2022-11-11 14:52:38 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { EmailEditorComponent } from 'angular-email-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'send-email';
+  title = 'angular-email-editor';
+
+  @ViewChild(EmailEditorComponent)
+  private emailEditor: EmailEditorComponent = new EmailEditorComponent;
+  
+  editorLoaded(event: any) {
+    console.log(event)
+    // load the design json here
+    // this.emailEditor.editor.loadDesign({});
+  }
+
+  exportHtml() {
+    this.emailEditor.editor.exportHtml((data: any) => console.log('exportHtml', data));
+  }
 }
